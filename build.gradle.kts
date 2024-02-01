1c4222db9ddd360316b6644a2960dc02fdbe58cb
Shadowfflare <henkwever24@gmail.com>
2024-02-01 18:59:35 +0100

    vulcanium

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.0.0"
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.10"
     java
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 repositories {
@@ -18,15 +18,16 @@ dependencies {
     implementation("org.avaje:ebean:2.7.3")
     implementation("com.google.guava:guava:10.0.1")
     implementation("commons-lang:commons-lang:2.3")
-    implementation(platform("com.intellectualsites.bom:bom-newest:1.40"))
+    implementation(enforcedPlatform("com.intellectualsites.bom:bom-newest:1.40"))
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+
     testImplementation("junit:junit:4.11")
     testImplementation("org.hamcrest:hamcrest-library:1.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-    compileOnly 'org.projectlombok:lombok:1.18.26'
-    annotationProcessor 'org.projectlombok:lombok:1.18.26'
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.6"
+    kotlinOptions.jvmTarget = "1.8"
 }
